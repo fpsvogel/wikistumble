ad86ea1a1c09bcef9fa921915d74f6b04bdb21fe
Felipe Vogel <felipe.vogel@sanabenefits.com>
2023-08-28 15:20:40 -0400

    Remove unused plugins

--- app/router.rb
+++ app/router.rb
@@ -3,22 +3,20 @@ require "open-uri"
 class Router < Roda
   opts[:root] = Config.root
 
+  plugin :route_csrf, require_request_specific_tokens: false, check_header: true
   plugin :sessions, secret: Config.secret_key
   plugin :assets, css: "app.css", js: "app.js", path: Config.root.join("public"), css_dir: "", js_dir: "", timestamp_paths: true
+  plugin :public
   plugin :render, views: Config.root.join("app/views")
   plugin :partials
-  plugin :route_csrf, require_request_specific_tokens: false, check_header: true
-  plugin :flash
-  plugin :link_to
-  plugin :path
-  plugin :status_303 # for Turbo
-  plugin :turbo
-  # plugin :forme_set, secret: Config.secret_key
   plugin :forme
   plugin :content_for
-  plugin :typecast_params
+  plugin :link_to
+  plugin :path
+  plugin :flash
+  plugin :turbo
+  plugin :status_303 # for Turbo
   plugin :enhanced_logger if Config.development?
-  plugin :public
 
   path(:root, "/")
   path(:next, "/next")
